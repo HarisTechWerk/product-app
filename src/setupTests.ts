299b6da5f3437a505a2dc666723c42c8433ca75a
Haris Qureshi <92890149+HarisTechWerk@users.noreply.github.com>
2025-03-02 16:25:35 +0100

    Fix TypeScript: add custom declaration for text-encoding-polyfill, update README

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,17 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from "text-encoding-polyfill";
+import { server } from "./mocks/server";
+
+// Polyfill TextEncoder and TextDecoder for Jest
+Object.assign(global, { TextEncoder, TextDecoder });
+
+
+// Establish API mocking before all tests
+beforeAll(() => server.listen({
+  onUnhandledRequest: 'bypass',
+}));
+
+// Reset any request handlers that we may add during the tests
+afterEach(() => server.resetHandlers());
+
+// Clean up after the tests are finished
+afterAll(() => server.close());
